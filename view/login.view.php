c12c302c22295fccc83e0c621a28063b568b05e8
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-09-30 15:30:11 -0300

    Subindo alterações CSS e HTML, também nomenclaturas, para melhor entendimento e arquitetura de código

--- view/login.view.php
+++ view/login.view.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Temperance Login</title>
     <link rel="shortcut icon" href="/view/images/sobriedade.ico" type="image/x-icon">
-    <link rel="stylesheet" href="style-login-conta.css">
+    <link rel="stylesheet" href="style-geral.css">
 </head>
 
 <body>
